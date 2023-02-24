bf9d539b65bd8bbe1b430fbbea52f251cbd048af
Cosik <mr.cosik@gmail.com>
2023-02-24 14:38:44 +0100

    finished DataHandler

--- data_explorer/data_tools.py
+++ data_explorer/data_tools.py
@@ -1,33 +1,36 @@
 import os
-from typing import List
+from datetime import datetime
 from uuid import uuid4
 
-import numpy as np
-import pandas as pd
 import petl as etl
 from django.conf import settings
 
 from data_explorer.models import Collection
+from data_explorer.swapi import ENDPOINTS
 
 
 class DataHandler:
     def __init__(self, file_name="", file_location=""):
-        self.file_name = (file_name or str(uuid4())) + ".csv"
+        self.file_name = f"{str(uuid4().hex)}.{file_name}.csv"
         self.file_location = file_location or os.path.join(settings.BASE_DIR, "csv")
         self.full_path = os.path.join(self.file_location, self.file_name)
 
-    def load_data(self):
-        return etl.fromcsv(self.full_path)
+    def load_data(self, full_path):
+        return etl.fromcsv(full_path)
 
-    def save_people_data(self, data, planets):
+    def save_people_data(self, data, planets, version_tag):
         data = self.parse_people_data(data, planets)
         etl.tocsv(data, self.full_path)
-        self._save_metadata()
+        self._save_metadata(version_tag, ENDPOINTS.people)
+        return data
 
     def parse_people_data(self, data, planets):
-        df = pd.DataFrame(data)
-        df["edited"] = pd.to_datetime(df["edited"])
-        df["data"] = df.loc[:, "edited"].apply(lambda x: x.strftime("%Y-%m-%d"))
+        df = etl.fromdicts(data)
+        df = etl.addfield(
+            df,
+            "date",
+            lambda x: datetime.fromisoformat(x["edited"]).strftime("%Y-%m-%d"),
+        )
         columns_to_drop = [
             "url",
             "starships",
@@ -36,38 +39,40 @@ class DataHandler:
             "films",
             "created",
             "edited",
+            "homeworld_url",
         ]
-        df.drop(columns_to_drop, axis=1, inplace=True)
-        df["homeworld"] = df["homeworld"].apply(
-            lambda x: self._resolve_planets(int(x.split("/")[-2]), planets)  # type: ignore
+        df = etl.rename(df, "homeworld", "homeworld_url")
+        df = etl.addfield(
+            df,
+            "homeworld",
+            lambda x: self._resolve_planets(
+                int(x["homeworld_url"].split("/")[-2]), planets
+            ),
         )
-        parsed_data = df.to_numpy()
-        parsed_data = np.insert(parsed_data, 0, df.columns, axis=0)
-        return parsed_data.tolist()
+        df = etl.cutout(df, *columns_to_drop)
+        return df
 
-    def save_planets_data(self, data):
+    def save_planets_data(self, data, version_tag):
         data = self._parse_planets_data(data)
         etl.tocsv(data, self.full_path)
-        self._save_metadata()
+        self._save_metadata(version_tag, ENDPOINTS.planets)
+        return data
 
-    def _parse_planets_data(self, data: List[dict]):
-        headers = data[0].keys()
-        parsed_data = [headers]
-        for row in data:
-            parsed_data.append(row.values())  # type: ignore
-        return parsed_data
+    def _parse_planets_data(self, data):
+        df = etl.fromdicts(data)
+        return df
 
     def _resolve_planets(self, id, planets):
-        try:
-            return planets[id - 1]["name"]
-        except IndexError:
-            return None
+        p = etl.cut(planets, "name")
+        return p[id][0]
 
-    def _save_metadata(self):
+    def _save_metadata(self, version_tag, endpoint):
         metadata = {
             "id": self.file_name.split(".")[0],
-            "file_name": self.file_name.split,
+            "file_name": self.file_name,
             "file_location": self.file_location,
+            "etag": version_tag,
+            "endpoint": endpoint,
         }
         collection_metadata = Collection(**metadata)
         collection_metadata.save()
@@ -76,4 +81,5 @@ class DataHandler:
         return Collection.objects.get(filename=filename)
 
     def count_values(self, table, columns):
-        return etl.valuecounts(table, *columns).cutout("frequency")
+        df = etl.valuecounts(table, *columns)
+        return etl.cutout(df, "frequency")
