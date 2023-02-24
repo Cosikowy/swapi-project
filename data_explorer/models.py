b77c507d3458bbc9b6c930df20175a989f802b22
Cosik <mr.cosik@gmail.com>
2023-02-24 14:40:50 +0100

    refactor collection model

--- data_explorer/models.py
+++ data_explorer/models.py
@@ -1,10 +1,21 @@
+import os
+
 from django.db import models
 
 
 class Collection(models.Model):
-    id = models.UUIDField("Collection ID")
+    id = models.UUIDField("Collection ID", primary_key=True)
     file_name = models.CharField("File Name", max_length=72)
     file_location = models.TextField("File Location")
     timestamp = models.DateTimeField(
         verbose_name="Download timestamp", auto_now_add=True
     )
+    etag = models.CharField("Version tag", max_length=80)
+    endpoint = models.CharField("Endpoint", max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.endpoint} {self.id}"
+
+    @property
+    def full_path(self):
+        return os.path.join(self.file_location, self.file_name)
--- data_explorer/views.py
+++ data_explorer/views.py
@@ -1,11 +1,22 @@
-# Create your views here.
-class Index:
-    ...
+from django.http import HttpResponse
+
+from data_explorer.data_tools import DataHandler
+from data_explorer.swapi import SWAPI
+
+sw = SWAPI("https://swapi.dev/api/")
 
 
-class History:
-    ...
+def update(request, *args, **kwargs):
+    planets_handler = DataHandler("planets")
+    planets, planets_version = sw.get_planets()
+    planets_data = planets_handler.save_planets_data(planets, planets_version)
+
+    people_handler = DataHandler("people")
+    people, people_version = sw.get_people()
+    people_handler.save_people_data(people, planets_data, people_version)
+
+    return HttpResponse("success")
 
 
-class TableExplorer:
-    ...
+def collecions_view(request, *args, **kwargs):
+    return HttpResponse({"status": 200})
